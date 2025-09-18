fc26c14d6e100c743211aa29ca8262615c624647
Sanjai-Magilan <sanjai6115@gmail.com>
2025-09-18 08:34:59 +0530

    schema done

--- BasicExpress/models/Schema.js
+++ BasicExpress/models/Schema.js
@@ -1,6 +1,9 @@
 import { Schema } from "mongoose";
+import mongoose from "mongoose";
 const DummySh = new Schema({
+  UserName: { type: String, require: true },
+  UserId: { type: Number, require: true, unique: true },
+  Role: { type: String, require: true, enum: ["user", "admin", "guest"] },
+});
 
-})
-
-export default DummySh
+export default mongoose.model("schem", DummySh);
