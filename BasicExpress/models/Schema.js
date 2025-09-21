c4f95485f5f72ca00bf125f558d6d07aececa461
Sanjai-Magilan <sanjai6115@gmail.com>
2025-09-21 21:40:42 +0530

    added user password in schema

--- BasicExpress/models/Schema.js
+++ BasicExpress/models/Schema.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 const DummySh = new mongoose.Schema({
   UserName: { type: String, required: true },
   UserId: { type: Number, required: true, unique: true },
+  UserPass: { type: String, required: true },
   Role: { type: String, required: true, enum: ["user", "admin", "guest"] },
 });
 
